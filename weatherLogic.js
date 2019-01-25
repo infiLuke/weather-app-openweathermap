df9532450d078afbd24b02031f10c7032dad7c9a
LukeJ <infiLuca@gmail.com>
2019-01-25 22:07:35 +0100

    refactor and reorganize

--- weatherLogic.js
+++ weatherLogic.js
@@ -3,18 +3,19 @@ const http = require('http');
 const colors = require('colors');
 const api = require('./api.json');
 
-function convertDate(timestamp) {
+function formatTimestamp(timestamp) {
   const months = [
     'Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'
   ];
-  const a = new Date(timestamp * 1000);
-  const year = a.getFullYear();
-  const month = months[a.getMonth()];
-  const date = a.getDate();
-  const hour = a.getHours();
-  const min = a.getMinutes();
-  const time = hour + ':' + min + ` (${date} ${month} ${year})`
-  return time;
+  const dateTime = new Date(timestamp * 1000);
+  const year = dateTime.getFullYear();
+  const month = months[dateTime.getMonth()];
+  const date = dateTime.getDate();
+  const hour = dateTime.getHours();
+  const min = dateTime.getMinutes();
+  const formattedDateTime = hour + ':' + min + ` (${date} ${month} ${year})`
+
+  return formattedDateTime;
 }
 
 // print error messages
@@ -24,18 +25,17 @@ function printError(error) {
 
 // format &amp; print important info from weather.json
 function printWeatherInfo(weather) {
-  // log out whole json file
-  // console.log(weather);
   // format and select relevant info
-  const weatherInfoTime = convertDate(weather.dt);
-  const sunriseTime = convertDate(weather.sys.sunrise);
-  const sunsetTime = convertDate(weather.sys.sunset);
-  const message = `The temperature in ${weather.name}, ${weather.sys.country} is ${weather.main.temp} degrees celsius with ${weather.weather[0].description}.\n` +
-      `  - ${weather.clouds.all}% cloud coverage and ${weather.main.humidity}% humidity\n` +
-      `  - a current low of ${weather.main.temp_min} and a high of ${weather.main.temp_max} celsius\n` +
-      `  - sunrise: ${sunriseTime}\n` +
-      `  - sunset:  ${sunsetTime}\n` +
-      `  - weather data was collected at ${weatherInfoTime}\n`
+  const weatherInfoTime = formatTimestamp(weather.dt);
+  const sunriseTime = formatTimestamp(weather.sys.sunrise);
+  const sunsetTime = formatTimestamp(weather.sys.sunset);
+  const message = `The temperature in ${weather.name}, ${weather.sys.country} is ${weather.main.temp} degrees celsius with ${weather.weather[0].description}.
+  - ${weather.clouds.all}% cloud coverage and ${weather.main.humidity}% humidity
+  - a current low of ${weather.main.temp_min} and a high of ${weather.main.temp_max} celsius
+  - sunrise: ${sunriseTime}
+  - sunset:  ${sunsetTime}
+  - weather data was collected at ${weatherInfoTime}`
+
   console.log(message.green);
   console.log(`  -----------------------------------------------------------`.cyan);
 }
@@ -45,6 +45,7 @@ function getWeatherData(city) {
   try {
     // connect
     const request = https.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=${api.key}`, response => {
+
       // check for http status code
       if (response.statusCode === 200) {
         let body = '';
@@ -63,6 +64,7 @@ function getWeatherData(city) {
         const statusCodeError = new Error(message);
         printError(statusCodeError);
       }
+
     });
     // handle error event for unresponsive url
     request.on('error', error => printError(error));
